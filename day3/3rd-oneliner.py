d1d0bfa2e0c8f94dc64e01e925271db006b9962b
dfrodrig <danielcr@ieee.org>
2024-12-03 13:44:54 +0000

    Overcoming deprecated

--- day3/3rd-oneliner.py
+++ day3/3rd-oneliner.py
@@ -1,4 +1,9 @@
 import re
+
 p=re.compile(r"mul\((\d+),(\d+)\)")
-print(sum ([int(x)*int(y) for x,y in p.findall(re.sub("don't\(\)(?s).*?do\(\)","",open(f"inputDay3.txt").read()))])   )
+#part 1
+print(f"Result part1: {sum ([int(x)*int(y) for x,y in p.findall(open(f'inputDay3.txt').read())])}")
+
+#part 2
+print(sum ([int(x)*int(y) for x,y in p.findall(re.sub("don't\(\).*?do\(\)","",open(f"inputDay3.txt").read(),flags=re.S))])   )
 
