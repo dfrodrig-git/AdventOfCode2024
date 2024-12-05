ee435f9cd7daa7a9f7bd842abf096e84a51dec9c
dfrodrig <danielcr@ieee.org>
2024-12-05 08:10:45 +0000

    first pass

--- day5/5th.py
+++ day5/5th.py
@@ -1,2 +1,54 @@
-#Advent Of Code 2024 day 5 
-#by dfrodrig-git 
+import re
+import logging
+import sys
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+testRun = True
+
+def assertExpected( result, expected,part=1):
+    logger.info(f'Result part:{part}: {result}, expected: {expected}')
+    if result != expected and testRun :
+        logger.critical("Unnaceptable!")
+        
+
+def getInput(test = True, day = 5) :
+    if testRun or test :
+        f=open(f'inputDay{day}Test.txt')
+    else :
+        f=open(f'inputDay{day}.txt')
+    
+    rules = [line.strip().split("|") for line in f if line.find("|") >0]
+
+
+    if testRun or test :
+        f=open(f'inputDay{day}Test.txt')
+    else :
+        f=open(f'inputDay{day}.txt')
+
+    data = [line.strip().split(",") for line in f if line.find(",")>0]
+
+    return rules, data
+
+class Rule() :
+    def __init__(before,after) :
+        self.bef = before
+        self.aft = after
+    def __str__() :
+        debug("Rule: {self.bef}|{self.aft}")
+    
+#Part1
+rulesInput, data = getInput()
+
+rules = [Rule(bef,aft) for bef,aft in rulesInput]
+
+
+#assertExpected(sum([x[2] for x in resultsPart1]), 18)
+
+#Part 2
+
+
+#assertExpected(sum([x[1] for x in resultsPart2]), 9, part=2)
+
+
