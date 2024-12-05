1c733857efd6c8cccf381a36648532c7b5b56645
dfrodrig <danielcr@ieee.org>
2024-12-05 18:52:00 +0000

    Solved all!

--- day5/5th.py
+++ day5/5th.py
@@ -1,11 +1,12 @@
 import re
 import logging
 import sys
-logger = logging.getLogger(__name__)
+from functools import cmp_to_key
 logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
-testRun = True
+testRun = False
 
 def assertExpected( result, expected,part=1):
     logger.info(f'Result part:{part}: {result}, expected: {expected}')
@@ -13,42 +14,79 @@ def assertExpected( result, expected,part=1):
         logger.critical("Unnaceptable!")
         
 
-def getInput(test = True, day = 5) :
-    if testRun or test :
+def getInput(test = None, day = 5) :
+    if testRun :
         f=open(f'inputDay{day}Test.txt')
     else :
         f=open(f'inputDay{day}.txt')
     
-    rules = [line.strip().split("|") for line in f if line.find("|") >0]
-
+    rules = [list(map(int, line.strip().split("|"))) for line in f if line.find("|") >0]
 
     if testRun or test :
         f=open(f'inputDay{day}Test.txt')
     else :
         f=open(f'inputDay{day}.txt')
 
-    data = [line.strip().split(",") for line in f if line.find(",")>0]
-
+    data = [list(map(int, line.strip().split(","))) for line in f if line.find(",")>0]
+    
     return rules, data
 
-class Rule() :
-    def __init__(before,after) :
-        self.bef = before
-        self.aft = after
-    def __str__() :
-        debug("Rule: {self.bef}|{self.aft}")
+
+class ConstraintMap() :
+    def __init__(self, rules) :
+        self.buildMap(rules)
+
+    def buildMap(self, rules):
+        self.before={}
+        self.after={}
+        for bef,aft in rules :
+            self.before.setdefault(aft, []).append(bef)
+            self.after.setdefault(bef, []).append(aft)
+        #logger.debug(f"Maps have been built: \nbefore:{self.before}, \nafter:{self.after}")
     
+    def compare(self, x,y):
+        #print(f'comparign {x}, {y}')
+        if y in self.after.get(x, []) :
+            return 1
+        if y in self.before.get(x, []) :
+            return -1
+        
+        return 0
+        
+def checkConstraints(value, setBef, setAft, cmap ) :
+    #logger.debug(f"Checking value:{value}, setBef:{setBef}, setAft:{setAft}")
+    if len (set(cmap.after.get(value, [])) & setBef ) > 0:
+        return False
+    if len(set(cmap.before.get(value,[])) & setAft ) > 0:
+        return False
+    return True
+
+def validateUpdate(updateList, constraintsMap) :
+    #logger.info(f"Validating {updateList}")
+    for i, v in enumerate(updateList) :
+        if checkConstraints(v, set(updateList[:i]), set(updateList[i+1:]), constraintsMap) == False:
+            return False
+    return updateList   
+
+def computeResult(validatedList) :
+    #logger.debug(f"validatedList: {validatedList}")
+    return sum([x[int(len(x)/2)] for x in validatedList if x is not False])
+
+
+rules, data = getInput(False)
+cmap = ConstraintMap(rules)
+
 #Part1
-rulesInput, data = getInput()
-
-rules = [Rule(bef,aft) for bef,aft in rulesInput]
-
-
-#assertExpected(sum([x[2] for x in resultsPart1]), 18)
+validatedLists = [validateUpdate(x, cmap) for x in data if x is not False]
+assertExpected(computeResult(validatedLists),143) 
 
 #Part 2
-
-
-#assertExpected(sum([x[1] for x in resultsPart2]), 9, part=2)
+unordered = [x for x in data if x not in validatedLists]
+#logger.debug(f"Unordered: {unordered} ")
+for x in unordered :
+    x.sort(key=cmp_to_key(cmap.compare))
+    #logger.debug(f'reordered:{x}')
+    
+assertExpected(computeResult(unordered),123, part=2) 
 
 
